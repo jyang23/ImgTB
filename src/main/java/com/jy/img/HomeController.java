711983ac527549410990fe3a5a3101d781b3665e
Justin <justinyang1000@hotmail.com>
2019-07-15 09:46:11 -0400

    Initial commit

---
+++ src/main/java/com/jy/img/HomeController.java
@@ -0,0 +1,13 @@
+package com.jy.img;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
